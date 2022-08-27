d7811167d7a99d11249dfbd98a6ef6f0ea7f02dc
weideg <112263547+weideg@users.noreply.github.com>
2022-08-28 00:14:19 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -30,6 +30,7 @@ export const config = {
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
+//你就是个大傻逼！！！！你妈白憨怂娃
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
